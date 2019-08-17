f5f8a008bfd49a1a605d728150fe61e6b4e44e12
larscom <larskniep@gmail.com>
2019-08-17 16:46:30 +0200

    added test url

--- e2e/src/test.ts
+++ e2e/src/test.ts
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 // tslint:disable-next-line: no-unused-expression
-fixture`Getting Started`.page`http://127.0.0.1`;
+fixture`Getting Started`.page`${process.env.NGRX_STORE_STORAGESYNC_URL}`;
 
 test('My first test', async t => {
   await t.expect(Selector('h2').innerText).eql('Local Storage');
