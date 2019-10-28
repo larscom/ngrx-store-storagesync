b2153f4ba62e60c368e501abafe7b4b3502bdac2
larscom <larskniep@gmail.com>
2019-10-28 20:39:46 +0100

    on master branch

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -1,9 +1,15 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "master" ]; then
+  mkdir -p projects/ngrx-store-storagesync/.git
 
-if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "auto-versioning" ]; then
-  git checkout $TRAVIS_BRANCH
-  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - Updated npm version"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
   git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/larscom/ngrx-store-storagesync.git"
+  git checkout master
+
+  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - Updated npm version"
+
+  sleep 5
+
   git push --follow-tags
 fi
