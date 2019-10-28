bb7dd97d3b9b906026b7a7199c5dcb5ee20e7a4c
larscom <larskniep@gmail.com>
2019-10-28 21:37:00 +0100

    fixed typo

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_PULL_REQUEST" == false ] && [ "$TRAVIS_BRANCH "== "master" ] && [ -z "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH " == "master" ] && [ -z "$TRAVIS_TAG" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
@@ -8,7 +8,7 @@ if [ "$TRAVIS_PULL_REQUEST" == false ] && [ "$TRAVIS_BRANCH "== "master" ] && [ 
   mkdir -p projects/ngrx-store-storagesync/.git
   git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/larscom/ngrx-store-storagesync.git"
 
-  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - ${TRAVIS_COMMIT_MESSAGE}"
+  npm version patch --prefix projects/ngrx-store-storagesync -m "#${TRAVIS_BUILD_NUMBER} - ${TRAVIS_COMMIT_MESSAGE}"
 
   sleep 1
 
