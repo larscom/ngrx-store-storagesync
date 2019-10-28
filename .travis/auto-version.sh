cf0caa89f7d5f24288ede26c8e8832073a20c31b
larscom <larskniep@gmail.com>
2019-10-28 16:17:22 +0100

    add auto version script

---
+++ .travis/auto-version.sh
@@ -0,0 +1,8 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "auto-versioning" ]; then
+  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - Updated npm version"
+  git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/larscom/ngrx-store-storagesync.git"
+  git push --follow-tags
+fi
