e63e99f30be760a8d24e9beeed1fc0718a2d1765
larscom <larskniep@gmail.com>
2019-10-28 21:00:30 +0100

    test

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -1,17 +1,21 @@
 if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "master" ]; then
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
+  git remote -v
+
+  git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+
   mkdir -p projects/ngrx-store-storagesync/.git
 
   git status
-
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
 
   git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/larscom/ngrx-store-storagesync.git"
   git checkout master
 
   git status
 
-  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - Updated npm version"
+  npm version patch --prefix projects/ngrx-store-storagesync -m "[ ${TRAVIS_BUILD_NUMBER} ] - ${TRAVIS_COMMIT_MESSAGE}"
 
   sleep 5
 
