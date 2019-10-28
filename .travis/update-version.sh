7d8e0a2c73f7b631148375b28c521901743e071b
larscom <larskniep@gmail.com>
2019-10-28 23:06:23 +0100

    check for npm version

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -1,4 +1,4 @@
-if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "master" ] && [ -z $TRAVIS_TAG ] && [[ $TRAVIS_COMMIT_MESSAGE != *"[Travis]"* ]]; then
+if [ $TRAVIS_PULL_REQUEST == false ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
@@ -8,7 +8,18 @@ if [ $TRAVIS_PULL_REQUEST == false ] && [ $TRAVIS_BRANCH == "master" ] && [ -z $
   mkdir -p projects/ngrx-store-storagesync/.git
   git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@github.com/${GITHUB_USERNAME}/ngrx-store-storagesync.git"
 
-  npm version patch --prefix projects/ngrx-store-storagesync -m "[Travis] - #${TRAVIS_BUILD_NUMBER} - ${TRAVIS_COMMIT_MESSAGE}"
+  # By default use npm version patch
+  version="patch"
+
+  if [ $TRAVIS_COMMIT_MESSAGE == *"[Minor]"* ]; then
+    version="minor"
+  fi
+
+  if [ $TRAVIS_COMMIT_MESSAGE == *"[Major]"* ]; then
+    version="major"
+  fi
+
+  npm version $version --prefix projects/ngrx-store-storagesync -m "[Travis] - #${TRAVIS_BUILD_NUMBER} - ${TRAVIS_COMMIT_MESSAGE}"
 
   sleep 1
 
