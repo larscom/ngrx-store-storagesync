ca54e945e1609fde1be2ec0e5c0fb3a86a368f36
larscom <larskniep@gmail.com>
2020-01-12 17:37:43 +0100

    auto merge with netlify branch

---
+++ scripts/netlify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+export GIT_COMMITTER_EMAIL='netlify@travis-ci.org'
+export GIT_COMMITTER_NAME='Travis CI'
+
+repo_dir="$(mktemp -d)"
+repo_name="ngrx-store-storagesync"
+repo_url="https://github.com/larscom/${repo_name}.git"
+repo_url_push="https://${GITHUB_SECRET}@github.com/larscom/${repo_name}.git"
+
+# merge master into netlify branch
+target_branch="netlify"
+
+git clone "${repo_url}" "${repo_dir}"
+
+echo "Change directory: ${repo_dir}"
+cd "${repo_dir}"
+
+echo "Checking out: ${target_branch}"
+git checkout "${target_branch}" >/dev/null 2>&1
+
+echo "Merging master into ${target_branch}"
+git merge master >/dev/null 2>&1
+
+echo "Pushing to ${repo_name}/${target_branch}"
+git push "${repo_url_push}" "${target_branch}" >/dev/null 2>&1
