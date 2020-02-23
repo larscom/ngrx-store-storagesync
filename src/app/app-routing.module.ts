a7da1b90332ed9d75d2de45a6eb6694b6cae91c0
larscom <larskniep@gmail.com>
2020-02-23 12:40:52 +0100

    enableIvy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: 'todo',
     pathMatch: 'full',
