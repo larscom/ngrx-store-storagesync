4763a30a349208122171a4eb73a678de2c786e49
larscom <larskniep@gmail.com>
2021-06-19 23:33:35 +0200

    update to angular 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,7 +60,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * Zone JS is required by default for Angular itself.
  */
 (window as any).__Zone_enable_cross_context_check = true;
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
