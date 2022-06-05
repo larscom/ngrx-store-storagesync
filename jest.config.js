c38c2953dfa0d907b3ff9d2d4d89e6d50730a71f
larscom <larskniep@gmail.com>
2022-06-05 15:19:49 +0200

    update packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
   roots: ['<rootDir>/projects/ngrx-store-storagesync'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
-  transform: {
-    '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular'
-  },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   setupFilesAfterEnv: ['<rootDir>/projects/ngrx-store-storagesync/src/test.ts'],
   coveragePathIgnorePatterns: ['models', 'public_api', 'test']
